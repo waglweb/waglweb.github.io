fb717d522f0cb4b4c718c3ae669df4af129c5159
Uk-Jin, Yang <ukjin1192@gmail.com>
2016-06-22 18:11:09 +0900

    Limint binding hover function only for desktop

--- static/js/waglpedia.js
+++ static/js/waglpedia.js
@@ -1,16 +1,20 @@
-// Show detail info on mouse over
-$(document).on('mouseover', '.waglpedia__service', function() {
-  var $waglpediaItem = $(this);
-  $waglpediaItem.find('.waglpedia__service-summary--default').addClass('hidden');
-  $waglpediaItem.find('.waglpedia__service-summary--active').removeClass('hidden');
-});
+// Only for desktop device
+if (typeof window.orientation === 'undefined') {
 
-// Hide detail info on mouse leave
-$(document).on('mouseleave', '.waglpedia__service', function() {
-  var $waglpediaItem = $(this);
-  $waglpediaItem.find('.waglpedia__service-summary--active').addClass('hidden');
-  $waglpediaItem.find('.waglpedia__service-summary--default').removeClass('hidden');
-});
+  // Show detail info on mouse over
+  $(document).on('mouseover', '.waglpedia__service', function() {
+    var $waglpediaItem = $(this);
+    $waglpediaItem.find('.waglpedia__service-summary--default').addClass('hidden');
+    $waglpediaItem.find('.waglpedia__service-summary--active').removeClass('hidden');
+  });
+
+  // Hide detail info on mouse leave
+  $(document).on('mouseleave', '.waglpedia__service', function() {
+    var $waglpediaItem = $(this);
+    $waglpediaItem.find('.waglpedia__service-summary--active').addClass('hidden');
+    $waglpediaItem.find('.waglpedia__service-summary--default').removeClass('hidden');
+  });
+}
 
 // Show detail info when spreading
 $(document).on('show.bs.collapse', '.waglpedia__service--collapsed', function () {
